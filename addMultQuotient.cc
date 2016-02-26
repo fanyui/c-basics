f7bf1f13bcd6f12df95cdd831bd0f29c7a1aab94
fanyui <fanyuiharisu@gmail.com>
2016-02-25 21:48:00 -0500

    addition multiply

---
+++ addMultQuotient.cc
@@ -0,0 +1,18 @@
+//a program to carry out addition multiplicaciton division 
+#include <iostream>//preprocessor directive
+using namespace std;
+int main(){
+	int numb1,numb2;
+	int sum, product,quotient;
+	cout<<"please input two numbers";
+	cin>>numb1>>numb2;
+	sum=numb1 + numb2;
+	product=numb1 * numb2;
+	quotient=numb1/numb2;
+	cout <<"the sum is "<<sum<<endl;
+	cout <<"the product is "<<product<<endl;
+	cout <<"the quotient is "<<quotient<<endl;
+
+return 0;
+
+}
